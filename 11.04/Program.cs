67ba18c5d5a4af6cc4a88c2a7fbe8a4d0830aac7
auyeskhanm <auyeskhanm@gmail.com>
2019-04-16 21:33:08 +0600

    Добавьте файлы проекта.

---
+++ 11.04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._04
+{
+    class Program
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["appConnection"].ConnectionString;
+        static SqlConnection sqlConnection = null;
+        static SqlCommand sqlCommand = null;
+        static SqlDataReader reader = null;
+
+        static void Main(string[] args)
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            sqlCommand = new SqlCommand ("create table gruppa (Id int primary key identity, Name varchar(50) not null)", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            Console.ReadKey();
+            reader.Close();
+        }
+    }
+}
